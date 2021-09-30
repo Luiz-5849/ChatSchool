aebc2e907d57e90186caa0f6be69916ab2b9b84e
Luiz <luizhenriqueemilianofeitoza@gmail.com>
2021-09-30 16:23:16 -0300

    Tive que alterar dnv porque deu um errinho

--- consulta.php
+++ consulta.php
@@ -16,6 +16,20 @@
             <option value="3">Cordenador(a)</option>
           </select>
           
+          <select name="escola">
+            <option selected value="0">Escola</option>
+            <option value="0">- -</option>
+            <?php
+                include 'conexao.php';
+
+                $comando=$con->prepare("select nome_escola from escolas");
+                $comando->execute();
+                while($linha = $comando->fetch(PDO::FETCH_OBJ)){
+                    echo '<option value="'. $linha->nome_escola .'">'. $linha->nome_escola .'</option>';
+                }
+            ?>
+          </select>
+
         <select name="acesso">
             <option selected value="0">Selecione</option>
             <option value="1">Aluno(a)</option>
