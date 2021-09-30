8fe4810ccf08b04d9b211ff47ff259170d99befa
Luiz <luizhenriqueemilianofeitoza@gmail.com>
2021-09-30 17:09:50 -0300

    Simplificação do codigo

--- consulta.php
+++ consulta.php
@@ -20,13 +20,7 @@
             <option selected value="0">Escola</option>
             <option value="0">- -</option>
             <?php
-                include 'conexao.php';
-
-                $comando=$con->prepare("select nome_escola from escolas");
-                $comando->execute();
-                while($linha = $comando->fetch(PDO::FETCH_OBJ)){
-                    echo '<option value="'. $linha->nome_escola .'">'. $linha->nome_escola .'</option>';
-                }
+                include 'cruds/option_escola.php';
             ?>
           </select>
 
