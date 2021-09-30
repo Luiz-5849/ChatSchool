b89a9d42fd3134c7ca9d818a80876ecbcb2519a0
Luiz <luizhenriqueemilianofeitoza@gmail.com>
2021-09-30 14:18:41 -0300

    Inserção desse script para conectar o primeiro_acesso.html ao cad.php

---
+++ scripts/cad.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+    $('#cadastrar').click(function(){
+        var dados = $('#form').serialize();
+
+        $.ajax ({
+            method: 'GET',
+            url: 'cruds/cad.php',
+            data: dados,
+        })
+
+        .done(function(msg)
+        {
+            if(msg == "y"){
+                location.href = 'cruds/verifica.php';
+            }
+            else{
+                $('#result').html("Esse apelido já existe, por favor insira outro");
+            }
+        })
+    })
+});
