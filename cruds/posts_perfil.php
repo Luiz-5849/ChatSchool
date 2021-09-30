07423e0aa5544f1f6e9009aa44c54ceaa182a2c4
Luiz <luizhenriqueemilianofeitoza@gmail.com>
2021-09-30 11:42:55 -0300

    Alterações para quando houver apenas uma imagem por post

--- cruds/posts_perfil.php
+++ cruds/posts_perfil.php
@@ -9,22 +9,36 @@ if ($comando->execute())
 {
 	if($comando->rowCount() > 0);
 	{
-		while($linha = $comando->fetch(PDO::FETCH_OBJ)){
+		while($linha = $comando->fetch(PDO::FETCH_OBJ))
+		{
 			$descricao = $linha->descricao_post;
-			echo $linha->descricao_post.'<br>';
 			$cod_post = $linha->cod_post;
+
 			$comandoII = $con->prepare("select imagem from imagens where cod_post = ?");
 			$comandoII->bindParam(1,$cod_post);
 			$comandoII->execute();
-			while($linhaII = $comandoII->fetch(PDO::FETCH_OBJ)){
+
+			if ($comandoII->rouCount() > 1) 
+			{
+				while($linhaII = $comandoII->fetch(PDO::FETCH_OBJ))
+				{
+					echo '<div class="col-3">
+							<img class="card-img-top" src="data:image/jpg;base64,' . base64_encode($linhaII->imagem) . '">
+						</div>';
+				}
+			} 
+			else
+			{
+				$img = $comandoII->fetch(PDO::FETCH_OBJ);
 				echo '<div class="col-3">
-						<img class="card-img-top" src="data:image/jpg;base64,' . base64_encode($linhaII->imagem) . '">
-					</div>';
+							<img class="card-img-top" src="data:image/jpg;base64,' . base64_encode($img->imagem) . '">
+						</div>';
 			}
 			
 		}
 	}
-	else{
+	else
+	{
 		echo "Sem postagens ainda";
 	}
 }
