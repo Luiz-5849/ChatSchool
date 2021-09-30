9a749d8234b427f0f96f161d0530e5de64ed7a4d
Luiz <luizhenriqueemilianofeitoza@gmail.com>
2021-09-30 16:18:31 -0300

    Alteração de consulta.html para php

---
+++ consulta.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+        <select name="acesso">
+            <option selected value="0">Tipo do perfil</option>
+            <option value="0">- -</option>
+            <option value="1">Aluno(a)</option>
+            <option value="2">Professor(a)</option>
+            <option value="3">Cordenador(a)</option>
+          </select>
+          
+        <select name="acesso">
+            <option selected value="0">Selecione</option>
+            <option value="1">Aluno(a)</option>
+            <option value="2">Professor(a)</option>
+            <option value="3">Cordenador(a)</option>
+          </select>
+    </form>
+</body>
+</html>
