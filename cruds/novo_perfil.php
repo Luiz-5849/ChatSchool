7322c9eca7ef43c48028b732ca3369071b5f524a
Renata <renata.botelho01@etec.sp.gov.br>
2021-10-21 16:06:08 -0300

    Criei o novo_perfil

---
+++ cruds/novo_perfil.php
@@ -0,0 +1,16 @@
+<?php
+
+include "conexao.php";
+    $email=$_POST['email'];
+    $senha=$_POST['senha'];
+    $acesso=$_POST['acesso'];
+
+    $comando=$con->prepare("insert into login (email,senha,acesso,primeiro_acesso) values (?,?,?,0)");
+    $comando->bindParam(1,$email);
+    $comando->bindParam(2,$senha);
+    $comando->bindParam(3,$acesso);
+
+    $comando->execute();
+
+    header("location:../administrativa.html");
+?>
