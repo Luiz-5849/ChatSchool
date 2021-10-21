22d029c22804d1ed5f7c38ff6eccddf1e97379ff
Renata <renata.botelho01@etec.sp.gov.br>
2021-10-21 16:21:05 -0300

    criei botão

--- cruds/novo_perfil.php
+++ cruds/novo_perfil.php
@@ -5,7 +5,7 @@ include "conexao.php";
     $senha=$_POST['senha'];
     $acesso=$_POST['acesso'];
 
-    $comando=$con->prepare("insert into login (email,senha,acesso,primeiro_acesso) values (?,?,?,0)");
+    $comando=$con->prepare("insert into login (login,senha,acesso,primeiro_acesso) values (?,?,?,0)");
     $comando->bindParam(1,$email);
     $comando->bindParam(2,$senha);
     $comando->bindParam(3,$acesso);
