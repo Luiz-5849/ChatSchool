2f72cee4772c633f44c759fbe162140356075b2e
Luiz <luizhenriqueemilianofeitoza@gmail.com>
2021-10-21 15:28:16 -0300

    Continuei o codigo

--- cruds/toPost.php
+++ cruds/toPost.php
@@ -4,10 +4,27 @@ include 'conexao.php';
 session_start();
 
 $cod = $_SESSION['cod_perfil'];
-$img = $_GET[''];
-$descricao = $_GET[''];
+$descricao = $_GET['descricao'];
+
+if (condition) {
+    $arquivo_atual = $_FILES['foto']['name'];
+}
 
 $comando = $con->prepare("insert into postagens (cod_perfil, descricao, hora_post, data_post) values (?, ?, curtime(), curdate())");
 $comando->bindParam(1, $cod);
 $comando->bindParam(2, $descricao);
 $comando->execute();
+
+if (condition) {
+    $comandoII = $con->prepare("select max(cod_post) from postagens where cod_perfil = ?");
+    $comandoII->bindParam(1, $cod);
+    $comandoII->execute();
+    $saida = $comandoII->fetch(PDO::FETCH_OBJ);
+    $cod_post = $saida->cod_post;
+
+    $imagem = file_get_contents("https://localhost/imagens/" . $arquivo_atual);
+    $comandoIII = $con->prepare("insert into imagens (imagem, nome_imagem) values (?, ?)");
+    $comandoIII->bindParam(1, $imagem);
+    $comandoIII->bindParam(2, $arquivo_atual);
+    $comandoIII->execute();
+}
