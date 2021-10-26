ba351775f119a5251a90245de5e9bc55f39fffa7
luiz <luizhenriqueemilianofeitoza@gmail.com>
2021-10-26 15:19:05 -0300

    alterações

--- cruds/novo_perfil.php
+++ cruds/novo_perfil.php
@@ -1,9 +1,12 @@
 <?php
 
 include "conexao.php";
+session_start();
+
     $email=$_POST['email'];
     $senha=$_POST['senha'];
     $acesso=$_POST['acesso'];
+    $acesso_user = $_SESSION['acesso'];
 
     $comando=$con->prepare("insert into login (login,senha,acesso,primeiro_acesso) values (?,?,?,0)");
     $comando->bindParam(1,$email);
@@ -12,5 +15,9 @@ include "conexao.php";
 
     $comando->execute();
 
+    if($acesso_user == 3){
+    header("location:../adm_co.html");
+    }elseif($acesso_user == 4){
     header("location:../administrativa.html");
+    }
 ?>
