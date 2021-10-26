a83ce2b33bd60d47e860c107fcbbd0be9078880d
luiz <luizhenriqueemilianofeitoza@gmail.com>
2021-10-26 17:22:59 -0300

    Coreção do conexao.php

--- cruds/conexao.php
+++ cruds/conexao.php
@@ -2,10 +2,9 @@
 
 $bco = 'bancotcc';
 $user = 'root';
-$senha = 'vertrigo';
-//Apagar em casa só a senha e a porta
+$senha = '';
 try{
-	$con = new PDO("mysql:host=localhost:3307; dbname=$bco", "$user", "$senha");
+	$con = new PDO("mysql:host=localhost; dbname=$bco", "$user", "$senha");
 	$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$con->exec("set names utf8");
 }
