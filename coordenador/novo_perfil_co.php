3072d1e576ce54976060be928824e70d4dcda7d3
Luiz <luizhenriqueemilianofeitoza@gmail.com>
2021-11-05 11:12:32 -0300

    Alteração de html para php

--- coordenador/novo_perfil_co.php
+++ coordenador/novo_perfil_co.php
@@ -7,7 +7,7 @@
         <form action="../cruds/novo_perfil.php" method="post">
             <div>
                 <label>E-mail</label>
-                <input type="email" name="email" id="email" placeholder="email">
+                <input type="text" name="email" id="email" placeholder="email">
             </div>
             <div>
                 <label>Senha</label>
@@ -18,7 +18,14 @@
                     <option selected>Selecione</option>
                     <option value="1">Aluno(a)</option>
                     <option value="2">Professor(a)</option>
-                  </select>
+                </select>
+            </div>
+            <div>
+                <select name="turma">
+                    <option selected>Selecione</option>
+                    <option value="null">--</option>
+                    <?php include '../cruds/option_turmas.php'; ?>
+                </select>
             </div>
             <div>
                 <input type="submit" value="Enviar">
