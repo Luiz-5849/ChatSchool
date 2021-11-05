1c4ae1411184c1467245be2f7c5c9c1f6c02d2a1
luiz <luizhenriqueemilianofeitoz@gmail.com>
2021-11-05 17:41:20 -0300

    Alteração no nome do arquivo e no formato

---
+++ coordenador/comunicados_escola.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comunicar</title>
+    
+</head>
+<body>
+    <label>Período</label>
+    <h1>COMUNICAR ESCOLA</h1>
+    <form name="" id="" action="../cruds/comunicar_escola.php" method="get">
+        <div>
+            <label>Comunicado</label>
+            <input type="text" name="comunicado">
+        </div>
+        <div>
+            <input type="submit" value="Comunicar">
+        </div>
+    </form>
+
+</body>
+</html>
