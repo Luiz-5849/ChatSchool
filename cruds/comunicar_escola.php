fbc0bac9e8090a7efd57b0682f564e51399882cc
luiz <luizhenriqueemilianofeitoza@gmail.com>
2021-11-09 14:40:42 -0300

    Alteração do código MySQL

--- cruds/comunicar_escola.php
+++ cruds/comunicar_escola.php
@@ -6,7 +6,7 @@ session_start();
 $escola = $_SESSION['cod_escola'];
 $comunicado = $_GET['comunicado'];
 
-$comando = $con->prepare("INSERT into comunicado (comunicado, cod_escola, data_comunicado) values (?, ?, curdate())");
+$comando = $con->prepare("INSERT into comunicados (comunicado, cod_escola, data_comunicado) values (?, ?, curdate())");
 $comando->bindParam(1, $comunicado);
 $comando->bindParam(2, $escola);
 $comando->execute();
