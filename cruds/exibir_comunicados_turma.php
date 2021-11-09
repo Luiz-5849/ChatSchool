b9e49d7f922b16b91e50cc10456b923bba8b2617
luiz <luizhenriqueemilianofeitoza@gmail.com>
2021-11-09 15:59:13 -0300

    Novos códigos

---
+++ cruds/exibir_comunicados_turma.php
@@ -0,0 +1,19 @@
+<?php
+
+include 'conexao.php';
+$cod_perfil = $_SESSION['cod_perfil'];
+
+$comando = $con->prepare("SELECT cod_turma from enturmando where cod_perfil = ?");
+$comando->bindParam(1, $cod_perfil);
+$comando->execute();
+while ($linha = $comando->fetch(PDO::FETCH_OBJ)){
+    $cod_turma = $linha->cod_turma;
+
+    $comandoII = $con->prepare("SELECT comunicado from comunicados where cod_turma = ?");
+    $comandoII->bindParam(1, $cod_turma);
+    $comandoII->execute();
+    
+    while($linhaII = $comandoII->fetch(PDO::FETCH_OBJ)){
+        echo '<div class="accordion-body">'. $linhaII->comunicado .'</div>';
+    }
+}
