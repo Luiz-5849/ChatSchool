a4a4531457221e970764248312967d488692bab3
Luiz <luizhenriqueemilianofeitoza@gmail.com>
2021-11-11 17:50:38 -0300

    Integração para o js

--- cruds/pesquisar_perfil.php
+++ cruds/pesquisar_perfil.php
@@ -4,21 +4,33 @@ include 'conexao.php';
 
 $pesquisado = "%".$_POST['pesquisar']."%";
 
-$comando=$con->prepare("SELECT cod_imagem,apelido,nome,cod_perfil FROM perfil where nome like ? or email like ? or apelido like ?");
+$comando=$con->prepare("SELECT perfil.apelido, perfil.nome, perfil.cod_perfil, imagens.nome_imagem FROM perfil inner join imagens on perfil.cod_imagem = imagens.cod_imagem where nome like ? or email like ? or apelido like ?");
 $comando->bindParam(1,$pesquisado);
 $comando->bindParam(2,$pesquisado);
 $comando->bindParam(3,$pesquisado);
 
 $comando->execute();
+$linha=$comando->fetch(PDO::FETCH_OBJ)
+$saida = json_encode($linha);
+echo $saida;
 
-while($linha=$comando->fetch(PDO::FETCH_OBJ)){
+
+/*while($linha=$comando->fetch(PDO::FETCH_OBJ)){
     $comandoII=$con->prepare("SELECT nome_imagem from imagens where cod_imagem=?");
     $comandoII->bindParam(1,$linha->cod_imagem);
     $comandoII->execute();
 
     $linhaII=$comandoII->fetch(PDO::FETCH_OBJ);
-    echo '<div><img src="'.$linhaII->nome_imagem.'"></div>
+    echo '
+    <form method="post" id="form" action="">
+    <a id="teste">
+    <div><img src="'.$linhaII->nome_imagem.'"></div>
     <div><p>'.$linha->apelido.'</p></div>
     <div><p>'.$linha->nome.'</p></div>
-    <form method="post" action="../cruds/seguir.php"><input type="submit" value="seguir"><input type="hidden" value="'.$linha->cod_perfil.'" name="cod_seguido"></form>';
-}
+    <input type="hidden" value="'.$linha->cod_perfil.'">
+    </a>
+    </form>
+    ';
+//}
+
+//<form method="post" action="../cruds/seguir.php"><input type="submit" value="seguir"><input type="hidden" value="'.$linha->cod_perfil.'" name="cod_seguido"></form>
