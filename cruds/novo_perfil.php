cc32261160a7ff05eb6ccf0001c628ec2303d687
Luiz-5849 <luizhenriqueemilianofeitoza@gmail.com>
2021-11-19 15:20:15 -0300

    Dividi a execução do codigo para cada user

--- cruds/novo_perfil.php
+++ cruds/novo_perfil.php
@@ -10,36 +10,41 @@ session_start();
     
     if ($acesso_user == 4){
         $escola = $_POST['escola'];
-    }else{
+
+        $comando=$con->prepare("insert into perfil (login, senha, acesso, cod_escola) values (?, ?, ?, ?)");
+        $comando->bindParam(1,$email);
+        $comando->bindParam(2,$senha);
+        $comando->bindParam(3,$acesso);
+        $comando->bindParam(4,$escola);
+        $comando->execute();
+
+        header("location:../programador/administrativa.html");
+
+    }elseif ($acesso_user == 3){
         $escola = $_SESSION['cod_escola'];
-    }
-    
-    $comando=$con->prepare("insert into perfil (login, senha, acesso, cod_escola) values (?, ?, ?, ?)");
-    $comando->bindParam(1,$email);
-    $comando->bindParam(2,$senha);
-    $comando->bindParam(3,$acesso);
-    $comando->bindParam(4,$escola);
-    $comando->execute();
-
-    if ($acesso_user == 3 && !isset($_POST['turma'])) {
-
         $cod_turma = $_POST['turma'];
 
-        $comandoII = $con->prepare("select cod_perfil from perfil where login = ? and senha = ?");
+        echo 'cod_turma: ' . $cod_turma;
+
+        $comando=$con->prepare("insert into perfil (login, senha, acesso, cod_escola) values (?, ?, ?, ?)");
+        $comando->bindParam(1,$email);
+        $comando->bindParam(2,$senha);
+        $comando->bindParam(3,$acesso);
+        $comando->bindParam(4,$escola);
+        $comando->execute();
+
+        $comandoII = $con->prepare("select cod_perfil from perfil where login = ?");
         $comandoII->bindParam(1, $email);
-        $comandoII->bindParam(2, $senha);
         $comandoII->execute();
-        $cod_perfil = $comandoII->fetch(PDO::FETCH_OBJ);
+        $saida = $comandoII->fetch(PDO::FETCH_OBJ);
+        $cod_perfil_pTurma = $saida->cod_perfil;
 
         $comandoIII = $con->prepare("insert into enturmando (cod_perfil, cod_turma) values (?, ?)");
-        $comandoIII->bindParam(1, $cod_perfil->cod_perfil);
+        $comandoIII->bindParam(1, $cod_perfil_pTurma);
         $comandoIII->bindParam(2, $cod_turma);
         $comandoIII->execute();
+
+        header("location:../coordenador/adm_co.html");
     }
 
-    if($acesso_user == 3){
-    header("location:../coordenador/adm_co.html");
-    }elseif($acesso_user == 4){
-    header("location:../programador/administrativa.html");
-    }
 ?>
