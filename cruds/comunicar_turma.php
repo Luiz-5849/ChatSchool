5a7b082b0838e8cc1100860b8453702558570bf9
Luiz-5849 <luizhenriqueemilianofeitoza@gmail.com>
2021-11-19 15:19:44 -0300

    Corrigi o cod SQL

--- cruds/comunicar_turma.php
+++ cruds/comunicar_turma.php
@@ -5,7 +5,7 @@ include 'conexao.php';
 $comunicado = $_POST['comunicado'];
 $cod_turma = $_POST['turma'];
 
-$comando = $con->prepare("INSERT into comunicados (cod_turma, comunicado) values (?, ?)");
+$comando = $con->prepare("INSERT into comunicados (cod_turma, comunicado, data_comunicado) values (?, ?, curdate())");
 $comando->bindParam(1, $cod_turma);
 $comando->bindParam(2, $comunicado);
 $comando->execute();
