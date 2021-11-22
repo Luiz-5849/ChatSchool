d7ddae821f6a0ccb0583467dabfb50d221df1717
Luiz-5849 <luizhenriqueemilianofeitoza@gmail.com>
2021-11-22 16:53:30 -0300

    Correção do cod para nao ativar caso o user não tenha selecionado uma img

--- cruds/cad_img.php
+++ cruds/cad_img.php
@@ -8,22 +8,26 @@ $cod = $_SESSION['cod_perfil'];
 $arquivo_atual = $_FILES['flImage']['name'];
 $arquivo_tmp = $_FILES['flImage']['tmp_name'];
 
-$destino = "../imagens/" . $arquivo_atual;
-move_uploaded_file($arquivo_tmp, $destino);
-
-$comando = $con->prepare("insert into imagens (nome_imagem) values (?)");
-$comando->bindParam(1, $arquivo_atual);
-$comando->execute();
-
-$comandoII = $con->prepare("select cod_imagem from imagens where nome_imagem = ? order by cod_imagem desc limit 1");
-$comandoII->bindParam(1, $arquivo_atual);
-$comandoII->execute();
-$linha = $comandoII->fetch(PDO::FETCH_OBJ);
-$cod_img = $linha->cod_imagem;
-
-$comandoIII = $con->prepare("update perfil set cod_imagem = ? where cod_perfil = ?");
-$comandoIII->bindParam(1, $cod_img);
-$comandoIII->bindParam(2, $cod);
-$comandoIII->execute();
-
+if($arquivo_atual != null || $arquivo_atual != ""){
+    
+    $destino = "../imagens/" . $arquivo_atual;
+    move_uploaded_file($arquivo_tmp, $destino);
+    
+    $comando = $con->prepare("insert into imagens (nome_imagem) values (?)");
+    $comando->bindParam(1, $arquivo_atual);
+    $comando->execute();
+    
+    $comandoII = $con->prepare("select cod_imagem from imagens where nome_imagem = ? order by cod_imagem desc limit 1");
+    $comandoII->bindParam(1, $arquivo_atual);
+    $comandoII->execute();
+    $linha = $comandoII->fetch(PDO::FETCH_OBJ);
+    $cod_img = $linha->cod_imagem;
+    
+    $comandoIII = $con->prepare("update perfil set cod_imagem = ? where cod_perfil = ?");
+    $comandoIII->bindParam(1, $cod_img);
+    $comandoIII->bindParam(2, $cod);
+    $comandoIII->execute();
+    
+    header("location:../aluno_professor/perfil.php");
+}
 header("location:../aluno_professor/perfil.php");
