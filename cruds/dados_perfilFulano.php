848c00d91ce88e199edf0f0829cd0fd21615a0b8
luiz <luizhenriqueemilianofeitoza@gmail.com>
2021-11-25 17:00:24 -0300

    novas interfaces

---
+++ cruds/dados_perfilFulano.php
@@ -0,0 +1,66 @@
+<?php
+
+include 'conexao.php';
+
+
+//Comando para puxar o nome e a bio
+$comandoI = $con->prepare("select apelido, bio from perfil where cod_perfil = ?");
+$comandoI->bindParam(1, $cod);
+$comandoI->execute();
+$dadosI = $comandoI->fetch(PDO::FETCH_OBJ);
+$nome = $dadosI->apelido;
+if ($dadosI->bio == null || $dadosI->bio == "") {
+    $bio = '<span class="">Sem mensagem de perfil</span>';
+}else{
+    $bio = $dadosI->bio;
+}
+
+//comando para puxar a quantidade de publicações
+$comandoII = $con->prepare("select cod_post from postagens where cod_perfil = ?");
+$comandoII->bindParam(1, $cod);
+$comandoII->execute();
+if ($comandoII->rowCount() > 0) {
+    $publicacoes = $comandoII->rowCount();
+}else{
+    $publicacoes = 0;
+}
+
+//Comando para puxar o numero de pessoas que o usuário está seguindo
+$comandoIII = $con->prepare("select * from seguidores where cod_perfil = ?");
+$comandoIII->bindParam(1, $cod);
+$comandoIII->execute();
+if ($comandoIII->rowCount() > 0) {
+    $seguindo = $comandoIII->rowCount();
+}else{
+    $seguindo = 0;
+}
+
+
+
+//Comando para puxar o numero de seguidores
+$comandoIV = $con->prepare("select * from seguidores where cod_doSeguido = ?");
+$comandoIV->bindParam(1, $cod);
+$comandoIV->execute(); 
+if ($comandoIV->rowCount() > 0) {
+    $seguidores = $comandoIV->rowCount();
+}else{
+    $seguidores = 0;
+}
+
+
+
+//Comando para puxar a foto do perfil
+$comandoV = $con->prepare("select imagens.nome_imagem from imagens inner join perfil on perfil.cod_imagem = imagens.cod_imagem where perfil.cod_perfil = ?");
+$comandoV->bindParam(1, $cod);
+$comandoV->execute();
+if($comandoV->rowCount()>0){
+    $dados = $comandoV->fetch(PDO::FETCH_OBJ);
+    if($dados->nome_imagem == null || $dados->nome_imagem == ""){
+        $imagem = "blank-profile-picture-973460__480.png";
+    }
+    $imagem = $dados->nome_imagem;
+}else{
+    $imagem = "blank-profile-picture-973460__480.png";
+}
+
+?>
---
+++ cruds/posts_perfilFulano.php
@@ -0,0 +1,28 @@
+<?php
+
+include 'conexao.php';
+
+$comando = $con->prepare("select descricao_post, cod_imagem from postagens where cod_perfil = ? order by data_post desc");
+$comando->bindParam(1, $cod);
+if ($comando->execute())
+{
+	if($comando->rowCount() > 0);
+	{
+		while($linha = $comando->fetch(PDO::FETCH_OBJ))
+		{   
+			$descricao = $linha->descricao_post;
+			$cod_imagem = $linha->cod_imagem;
+
+			$comandoII = $con->prepare("select nome_imagem from imagens where cod_imagem = ?");
+			$comandoII->bindParam(1,$cod_imagem);
+			$comandoII->execute();
+
+			$img = $comandoII->fetch(PDO::FETCH_OBJ);
+			
+			echo '<div class="col-3"><img src="../imagens/' . $img->nome_imagem . '"></div>';
+		}
+	}
+}
+
+
+?>
