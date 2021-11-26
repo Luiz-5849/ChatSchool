d0eba22f5a1e124256428543cd01388d7fbc3376
Luiz-5849 <luizhenriqueemilianofeitoza@gmail.com>
2021-11-26 15:20:05 -0300

    Correções

--- cruds/posts_perfil.php
+++ cruds/posts_perfil.php
@@ -3,27 +3,24 @@
 include 'conexao.php';
 $cod = $_SESSION['cod_perfil'];
 
-$comando = $con->prepare("select descricao_post, cod_imagem from postagens where cod_perfil = ? order by data_post desc");
-$comando->bindParam(1, $cod);
-if ($comando->execute())
+$com = $con->prepare("select cod_imagem from postagens where cod_perfil = ? order by data_post desc");
+$com->bindParam(1, $cod);
+$com->execute();
+
+if($com->rowCount() > 0);
 {
-	if($comando->rowCount() > 0);
-	{
-		while($linha = $comando->fetch(PDO::FETCH_OBJ))
-		{   
-			$descricao = $linha->descricao_post;
-			$cod_imagem = $linha->cod_imagem;
+	while($lin = $com->fetch(PDO::FETCH_OBJ))
+	{   
+		//$descricao = $linha->descricao_post;
+		
+		if($lin->cod_imagem != "" || $lin->cod_imagem != null){
+			$comII = $con->prepare("select nome_imagem from imagens where cod_imagem = ?");
+			$comII->bindParam(1, $lin->cod_imagem);
+			$comII->execute();
 
-			$comandoII = $con->prepare("select nome_imagem from imagens where cod_imagem = ?");
-			$comandoII->bindParam(1,$cod_imagem);
-			$comandoII->execute();
-
-			$img = $comandoII->fetch(PDO::FETCH_OBJ);
-			
+			$img = $comII->fetch(PDO::FETCH_OBJ);
 			echo '<div class="col-3"><img src="../imagens/' . $img->nome_imagem . '"></div>';
 		}
 	}
 }
-
-
 ?>
