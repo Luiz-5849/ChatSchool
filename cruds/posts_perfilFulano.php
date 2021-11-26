9e37937162dc2029ee39550424010483fd13a034
Luiz-5849 <luizhenriqueemilianofeitoza@gmail.com>
2021-11-26 15:21:22 -0300

    Melhoramento do cod

--- cruds/posts_perfilFulano.php
+++ cruds/posts_perfilFulano.php
@@ -2,27 +2,29 @@
 
 include 'conexao.php';
 
-$comando = $con->prepare("select descricao_post, cod_imagem from postagens where cod_perfil = ? order by data_post desc");
+$comando = $con->prepare("select cod_imagem from postagens where cod_perfil = ? order by data_post desc");
 $comando->bindParam(1, $cod);
-if ($comando->execute())
-{
-	if($comando->rowCount() > 0);
-	{
-		while($linha = $comando->fetch(PDO::FETCH_OBJ))
-		{   
-			$descricao = $linha->descricao_post;
-			$cod_imagem = $linha->cod_imagem;
+$comando->execute();
 
+if($comando->rowCount() > 0);
+{
+	while($linha = $comando->fetch(PDO::FETCH_OBJ))
+	{   
+		//$descricao = $linha->descricao_post;
+		$cod_imagem = $linha->cod_imagem;
+
+		if($cod_imagem == null || $cod_imagem = ""){
+			
+		}else{
 			$comandoII = $con->prepare("select nome_imagem from imagens where cod_imagem = ?");
 			$comandoII->bindParam(1,$cod_imagem);
 			$comandoII->execute();
-
 			$img = $comandoII->fetch(PDO::FETCH_OBJ);
-			
 			echo '<div class="col-3"><img src="../imagens/' . $img->nome_imagem . '"></div>';
 		}
 	}
 }
+
 
 
 ?>
